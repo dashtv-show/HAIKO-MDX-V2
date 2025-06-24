63a1db7bf6755e8ffecd535256d1e9bc37fe7353
dashtv-show <dashtvshow7@gmail.com>
2025-06-23 22:08:03 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "HAIKO~MDX~Ad4kVCDA#FWAwOhWykS1FZ9xwnqkVF173-7r6WfmhmdTzp30bWgI",
+SESSION_ID: process.env.SESSION_ID || "HAIKO~MDX~VEonwZCZ#So2Lq-QRUQaIKTKlTdGVn9FXRUGc5bXHQZRM_yW3kBo",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
@@ -37,7 +37,7 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "18293511531",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "18292748112",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "mr dacheno",
 // add bot owner name
