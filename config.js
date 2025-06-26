080d53d9a7e06bd0560210b52cd500e3cfde53db
Save <dashtvshow7@gmail.com>
2025-06-26 12:11:46 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "HAIKO~MDX~ZQgzjLxK#GRXzqFhK6YVXjfQ0JzzvsEiNQSNEUVxGm3-Fpr9EO0c",
+SESSION_ID: process.env.SESSION_ID || "HAIKO~MDX~PY1RVbzI#FsFied83Uy53GLWYVlX6wS1gSsl6-juNjDpi2qS2Mfw",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
@@ -37,9 +37,9 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "18292748112",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "18293511531",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "✰mr dacheno ✞𝗩𝗢𝗜𝗗",
+OWNER_NAME: process.env.OWNER_NAME || "𓃵𝛭ꝛ -𝐃𝛥𝑆𝛨𝛯𝚴𝛩𓃵",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅᴇᴠ xᴛʀᴇᴍᴇ*",
 // add bot owner name    
@@ -65,7 +65,7 @@ AUTO_REPLY: process.env.AUTO_REPLY || "false",
 // make true or false automatic text reply 
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
 // maks true for always online 
-PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
+PUBLIC_MODE: process.env.PUBLIC_MODE || "false",
 // make false if want private mod
 AUTO_TYPING: process.env.AUTO_TYPING || "false",
 // true for automatic show typing   
