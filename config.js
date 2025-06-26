aea2c67dae82ff0cf6de2a6fea1e44f08ea53116
Save <dashtvshow7@gmail.com>
2025-06-26 12:33:43 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
-function convertToBool(text, fault = 'true') {
+function convertToBool(text, fault = 'false') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "HAIKO~MDX~PY1RVbzI#FsFied83Uy53GLWYVlX6wS1gSsl6-juNjDpi2qS2Mfw",
+SESSION_ID: process.env.SESSION_ID || "HAIKO~MDX~DBlWQboB#eU2GIQEfoIO4CaxH9twjcX7Ky7ahsD5LlC6k0TaoHMM",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "false",
 // make true or false status auto seen
